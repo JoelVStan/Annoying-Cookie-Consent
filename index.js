416168912b195f6894204499ce202ef36e1e0ff0
Joel V Stanley <101164781+JoelVStan@users.noreply.github.com>
2023-07-24 21:13:50 +0530

    The App is ready

--- index.js
+++ index.js
@@ -2,18 +2,26 @@ const modalEl = document.getElementById('modal')
 const closeButton = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
 const modalText = document.getElementById('modal-text')
-document.getElementById('modal-inner')
+const declineBtn = document.getElementById('decline-btn')
+const modalChoiceBtn = document.getElementById('modal-choice-btn')
 
 setTimeout(function() {
     modalEl.style.display = 'inline'
-}, 1500)
+}, 2000)
 
 closeButton.addEventListener('click', function() {
     modalEl.style.display = 'none'
 })
 
+declineBtn.addEventListener('click', function() {
+    modalChoiceBtn.classList.toggle('modal-btn-reverse')
+})
+
 consentForm.addEventListener('submit', function(e) {
     e.preventDefault()
+
+    const consentFormData = new FormData(consentForm)
+    const consentFormDataName = consentFormData.get('fullName')
 
     modalText.innerHTML = `
     <div class="modal-inner-loading">
@@ -28,14 +36,16 @@ consentForm.addEventListener('submit', function(e) {
         document.getElementById('upload-text').innerText = `Making the sale..🤑💰`
     }, 2000);
 
+
     setTimeout(function() {
         document.getElementById('modal-inner').innerHTML = `
-        <h2 class="text-[30px] font-bold mt-5 mb-5">Thanks for your info! </h2>
+        <h2 class="text-[30px] font-bold mt-5 mb-5">Thanks <span class="text-fuchsia-500 text-[32px]">${consentFormDataName}</span>, you fool! </h2>
         <p class="modal-text">We just sold the rights to your eternal soul.</p>
         <div class="idiot-gif">
             <img src="images/laugh.gif" class="shadow-[1px_1px_3px_gray] rounded">
         </div>
         `
+        closeButton.disabled = false
     }, 3000);
 
 })
